34e3ec8e147c1a52df9d62822710ff752939997d
Jyotiraditya <jyotiraditya182@gmail.com>
2018-09-30 04:36:54 +0000

    Fixed my some sweet derps
    Signed-off-by: Jyotiraditya <jyotiraditya182@gmail.com>

--- src/com/wave/settings/preferences/Utils.java
+++ src/com/wave/settings/preferences/Utils.java
@@ -39,7 +39,7 @@ import android.view.Surface;
 import android.view.WindowManager;
 
 public final class Utils {
-    private static final String TAG = "NitrogenSettingsUtils";
+    private static final String TAG = "WaveSettingsUtils";
 
     // Device types
     private static final int DEVICE_PHONE = 0;
